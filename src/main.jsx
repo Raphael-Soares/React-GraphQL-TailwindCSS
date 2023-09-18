9e0950c1b32cffd94b420d651de9de1df773a962
Raphael-Soares <soaresphaell@gmail.com>
2023-09-18 16:37:00 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import { ApolloProvider } from "@apollo/client";
+import { client as ApolloClientConfig } from "./lib/apollo";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ApolloProvider client={ApolloClientConfig}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
